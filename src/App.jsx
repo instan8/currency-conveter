8209296a9dcb68361a4b238c221cfeccfefddac4
unknown <adityakrishna585@gmai.com>
2025-09-01 13:34:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+import { FaArrowAltCircleRight } from "react-icons/fa";
+import DropDown from './components/DropDown';
+
+
+function App() {
+   
+  const apiKey = import.meta.env.VITE_API_KEY;
+    const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("INR");
+  const[conversionResult,setConversionResult] = useState(0)
+    const [amount,setamount]=useState(0);
+  async function getExchange(fromCurrency,toCurrency,amount){
+    console.log(amount,"amount")
+ const res= await fetch( `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${fromCurrency}/${toCurrency}/${amount}`)
+ const data = await res.json()
+ setConversionResult(data.conversion_result)
+ console.log(data,"data")
+  }
+ return (
+    <div className='h-screen w-screen flex justify-center items-center bg-stock-image bg-cover '>  
+    <div className='h-[30vh]  md:h-[50vh] lg:h-[50vh] w-[50vw] bg-gray-200 rounded-lg relative px-6 flex flex-col gap-1 sm:gap3 backdrop-blur-md shadow-lg bg-white/30 border border-white/20'>
+        <h1 className='font-bold text-sm sm:text-lg mb-2'> Currency Converter </h1>
+        <section className='flex justify-between '> 
+          <div className='w-[30%] text-sm sm:text-lg'> 
+            <p className='font-semibold'>From:</p>
+              <DropDown  selectedCurrency={fromCurrency} currencySetter={setFromCurrency}/>
+              </div>
+              <FaArrowAltCircleRight className='self-center'/>
+          <div className='w-[30%] text-sm'> 
+            <p className='font-semibold text-sm sm:text-lg'>To:</p>
+              <DropDown selectedCurrency={toCurrency} currencySetter={setToCurrency}/>
+              </div>
+             
+                 </section>
+      
+        <input type="number" placeholder='Enter Amount' className='border-2 rounded-md border-black w-full' onChange={(e)=>{setamount(e.currentTarget.value)}}/>
+        <p className='text-green-500 text-sm sm:text-lg'>Your Amount After Converting: {conversionResult}</p>
+  <button className='self-end bg-[#191970] text-white w-full h-10  sm:w-[20%] rounded-md' onClick={()=>getExchange(fromCurrency,toCurrency,amount)}>Convert</button>
+    </div>
+    </div>
+ )
+}
+
+export default App
---
+++ src/components/DropDown.jsx
@@ -0,0 +1,45 @@
+import React,{useState,useEffect} from 'react'
+import { IoMdArrowDropdown } from "react-icons/io";
+import { IoMdArrowDropup } from "react-icons/io";
+
+function DropDown({selectedCurrency,currencySetter}) {
+
+const [currencies,setCurrencies] = useState([]);
+    const [isOpen,setIsOpen] = useState(false);
+  const[isDropdown,setIsDropdown] = useState(true);
+   
+  const apiKey = import.meta.env.VITE_API_KEY;
+  
+ useEffect(()=>{
+  const currencyList =async()=>{
+const res= await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/codes`);
+const data = await res.json();
+let currencyCodes = data.supported_codes.slice(0,10).map(code=>code[0]);
+
+
+setCurrencies(currencyCodes);
+console.log(data.supported_codes.slice(0,10));
+  }
+  currencyList();
+ }
+,[])
+  return (
+    <div className='relative w-full  mb-4'>
+      <div className='relative' onClick={()=>{setIsDropdown(!isDropdown)}}> 
+      <button className='bg-gray-300 text-sm w-full text-left px-0 sm:px-2' onClick={()=>{setIsOpen(!isOpen)}}>{selectedCurrency}</button>
+      <IoMdArrowDropdown className={`absolute top-1 right-1 ${isDropdown?"block":"hidden"} cursor-pointer`} />
+      <IoMdArrowDropup className={`absolute top-1 right-1 ${isDropdown?"hidden":"block"} cursor-pointer`} />
+
+      
+      </div>
+      <ul className={`absolute w-full list-none left-0 top-100% ${isOpen?"block":"hidden"}`} >
+        {
+          currencies.map((currency,index)=><li className=' border-2 text-sm border-gray-500 bg-gray-600 hover:bg-gray-400 cursor-pointer ' onClick={()=>{setIsOpen(false);currencySetter(currency.replace(/\s+/g, ""))}}>{currency}</li>)
+        }
+        
+      </ul>
+    </div>
+  )
+}
+
+export default DropDown
